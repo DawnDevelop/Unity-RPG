29ebd83d5c1a35e469f16b0103bb21095983208b
DawnDevelop <denizjoecks@yahoo.de>
2017-10-14 17:40:30 +0200

    First Commit

---
+++ UnityRPG/Assets/World Objects/BrightenLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightenLight : MonoBehaviour {
+
+    [SerializeField] Light lightToSwitch = null;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        lightToSwitch.intensity = 1;
+        lightToSwitch.range = 1;
+        lightToSwitch.color = new Color(0.28f,0.17f,0.76f,1);
+        RenderSettings.ambientIntensity = 1.5f;
+    }
+}
---
+++ UnityRPG/Assets/World Objects/DimLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimLight : MonoBehaviour {
+
+
+    [SerializeField] Light lightToSwitch = null;
+	// Use this for initialization
+	void Start ()
+    {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        lightToSwitch.range = 0;
+        lightToSwitch.intensity = 0;
+        lightToSwitch.color = new Color(-255f, -255f, -255f, 1);
+        RenderSettings.ambientIntensity = 0f;
+        RenderSettings.fogMode = FogMode.Linear;
+    }
+}
