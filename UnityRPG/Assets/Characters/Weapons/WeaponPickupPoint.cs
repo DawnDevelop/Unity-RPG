3658b152d16688b16003be48eeb3f53eba3090e0
DawnDevelop <denizjoecks@yahoo.de>
2017-10-14 16:42:58 +0200

    First Commit

---
+++ UnityRPG/Assets/Characters/Weapons/WeaponPickupPoint.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Characters;
+
+namespace RPG.Weapons
+{
+    [ExecuteInEditMode]
+    public class WeaponPickupPoint : MonoBehaviour
+    {
+
+        [SerializeField] Weapon weaponConfig;
+        [SerializeField] AudioClip pickUpSFX;
+
+        AudioSource audioSource;
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        void DestroyChildren()
+        {
+            foreach (Transform child in transform)
+            {
+                DestroyImmediate(child.gameObject);
+            }
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(!Application.isPlaying)
+            {
+                DestroyChildren();
+                InstantiateWeapon();
+            }
+        }
+
+        private void InstantiateWeapon()
+        {
+            var weapon = weaponConfig.GetWeaponPrefab();
+            weapon.transform.position = Vector3.zero;
+            Instantiate(weapon, gameObject.transform);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            FindObjectOfType<PlayerMovement>().PutWeaponInHand(weaponConfig);
+            audioSource.PlayOneShot(pickUpSFX);
+        }
+    }
+
+}
